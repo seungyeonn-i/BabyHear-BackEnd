304d5a50cd1ac46cd4221891cf92fd8081350cf6
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-06-05 11:52:34 +0900

    fix : CORS 설정

--- src/main/java/babyhere/server/controller/MainController.java
+++ src/main/java/babyhere/server/controller/MainController.java
@@ -4,16 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @RestController
 @AllArgsConstructor
 @Slf4j
+@CrossOrigin(origins = "*")
 public class MainController {
 
 
@@ -29,9 +34,24 @@ public class MainController {
         Process process = Runtime.getRuntime().exec(command);
         int exitCode = process.waitFor();
 
+
         if (exitCode == 0) {
             // 명령어가 성공적으로 실행되었을 때의 처리
-            return new ResponseEntity<>(HttpStatus.OK);
+
+            Process processOut = Runtime.getRuntime().exec("cat /home/admin/cry/baby_cry_detection/prediction/prediction.txt");
+            InputStream inputStream = processOut.getInputStream();
+            InputStream errorStream = processOut.getErrorStream();
+
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            StringBuilder output = new StringBuilder();
+
+            while ((line = inputReader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+            System.out.println(output.toString());
+
+            return new ResponseEntity<>(output.toString(),HttpStatus.OK);
         } else {
             // 명령어 실행 중 오류가 발생했을 때의 처리
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -49,7 +69,21 @@ public class MainController {
 
         if (exitCode == 0) {
             // 명령어가 성공적으로 실행되었을 때의 처리
-            return new ResponseEntity<>(HttpStatus.OK);
+
+            Process processOut = Runtime.getRuntime().exec("cat /Users/jeong-seungyeon/codes/server/a.txt");
+            InputStream inputStream = processOut.getInputStream();
+            InputStream errorStream = processOut.getErrorStream();
+
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            StringBuilder output = new StringBuilder();
+
+            while ((line = inputReader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+            System.out.println(output.toString());
+
+            return new ResponseEntity<>(output.toString(),HttpStatus.OK);
         } else {
             // 명령어 실행 중 오류가 발생했을 때의 처리
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
